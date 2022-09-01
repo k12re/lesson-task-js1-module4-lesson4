65ed24090698f056167aeff97281902428b095a6
Ken Thore <kenthore@live.no>
2022-09-01 14:52:33 +0200

    checked agains answer and corrected and refactored some lines

--- script.js
+++ script.js
@@ -2,55 +2,47 @@ const contactForm = document.querySelector("#contactForm");
 const firstName = document.querySelector("#firstName")
 const lastName = document.querySelector("#lastName")
 const email = document.querySelector("#email")
-let button = document.querySelector(".button")
-const message = document.querySelector(".message")
+const button = document.querySelector("button")
+const message = document.querySelector("#message")
 
 
-function validateForm() {
+function checkIfButtonIsDisabled() {
+    // if all inputs pass validation enable the button
+    if (checkLen(firstName.value , 1) && checkLen(lastName.value , 4) && validateEmail(email.value)) {
+        button.disabled = false;
+    }
+    else {
+        // clear the message
+        message.innerHTML = "";
+        // disable button
+        button.disabled = true;
+    }
+}
+
+// call the same function for each input's keyup event
+firstName.addEventListener("keyup", checkIfButtonIsDisabled);
+lastName.addEventListener("keyup", checkIfButtonIsDisabled);
+email.addEventListener("keyup", checkIfButtonIsDisabled);
+
+// function to run when the form is submitted
+function validateForm(event) {
     
     event.preventDefault()
+    // display a message once the form has been submitted
+    message.innerHTML = `<div class="message">Your message has been sent</div>`
 
-    if (checkLen(firstName.value, 0) === true ){
-        console.log(firstName.value.length)
-    }
-    else {
-        console.log("Please type first name")
-    }
-    
-
-    if (checkLen(lastName.value, 3) === true) {
-        console.log(lastName.value.length)
-    }
-    else {
-        console.log("Please type last name")
-    }
- 
-
-    if (validateEmail(email.value) === true) {
-        console.log(email.value)
-    }
-    else {
-        console.log("Please type email")
-    }
-
-    if (firstName.value.length > 0 && lastName.value.length > 3 && email.value === true) {
-        message.display = "block"
-        button.disabled = "false"
-    }
-    else {
-        //button.disabled = "true"
-    }
-
+    contactForm.reset();
 }
 
 contactForm.addEventListener("submit", validateForm)
 
+// function to check if the length of the input value is valid
 function checkLen (value, len) {
-    if (value.length > len) {
-        return true
+    if (value.length >= len) {
+        return true;
     }
     else {
-        return false
+        return false;
     }
 }
 
